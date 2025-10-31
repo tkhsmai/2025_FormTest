7ba3ed4ac33588053f560cdf9b812575f301df43
maimane04@icloud.com <Gorimane13>
2025-10-31 14:32:48 +0900

    3*3ボタンの作成

--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -16,36 +16,37 @@ namespace Form_Test
         public Form1()
         {
             InitializeComponent();
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < 3; j++)
                 {
                     //インスタンスの作成
-                    TestButton testButton = new TestButton();
+                    TestButton testButton = new TestButton(new Point(i * 50, j * 50), new Size(50, 50), "TestButton");
 
                     //ボタンの位置を設定
-                    testButton.Location = new Point(i * 90, j * 50);
+                    //testButton.Location = new Point(i * 90, j * 50);
 
                     //ボタンの大きさ設定
-                    testButton.Size = new Size(80, 40);
+                    //testButton.Size = new Size(80, 40);
 
                     //ボタンの名前設定
-                    testButton.Text = "TestButton";
+                    //testButton.Text = "TestButton";
 
                     //ボタンをクリックしたときに何を出力するか
-                    //ここでは下の処理の"Form始動！"という言葉が出力される
-                    testButton.MouseHover += hogehogeClick;
+                    //ここでは下の処理の"クリックされてしまいました"という言葉が出力される
+                    //testButton.MouseHover += hogehogeClick;
 
                     //コントロールにボタンを追加
                     Controls.Add(testButton);
                 }
-            }
+            }   
         }
 
-        private void hogehogeClick(object sender, EventArgs e)
-        {
-            MessageBox.Show("クリックされてしまいました");
-        }
+        //private void hogehogeClick(object sender, EventArgs e)
+        //{
+        //    MessageBox.Show("クリックされてしまいました");
+        //}
+
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Form始動！");
--- Form_Test/Form_Test/TestButton.cs
+++ Form_Test/Form_Test/TestButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace Form_Test
 {
     internal class TestButton : Button
     {
-       
+       public TestButton(Point position, Size size, string text)
+        {
+            //ボタンの位置を設定
+            Location = position;
+
+            //ボタンの大きさ設定
+            Size = size;
+
+            //ボタンの名前設定
+            Text = text;
+
+            //ボタンをクリックしたときに何を出力するか
+            Click += ClickEvent;
+        }
+
+    // クリックしたときの出力内容を自分で設定
+    private void ClickEvent(object sender, EventArgs e)
+        {
+            MessageBox.Show("TestButton内で設定");
+        }
     }
 }
